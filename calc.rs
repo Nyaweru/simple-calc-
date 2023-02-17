95ea11fa6971526c225083d4556c68c283400e3a
Nyaweru Wangari <102993985+Nyaweru@users.noreply.github.com>
2023-02-17 19:15:53 +0300

    Add files via upload

---
+++ calc.rs
@@ -0,0 +1,32 @@
+use std::io;
+fn main() {
+    println!("Welcome student.This is a simple calculator app");
+    
+}
+fn another_function() {
+    let  mut x=9;
+    println!("please enter the value of x");
+    io::stdin()
+    .read_line( &mut  ;x);
+
+    println!("the value of x is {x}")
+    let mut y=10;
+    println!("please enter the value of y");
+    io::stdin()
+    .read_line(&mut ;y );
+
+    println!("the value of y is; {y}");
+
+    
+    let sum = x+y;
+    println!("the sum is {x}, {y}");
+    let difference  = x- y;
+    println!("the difference is {x},{y}")
+}
+
+
+
+
+
+ 
+ 
